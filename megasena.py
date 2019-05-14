cf3cb3a97a94e11a3e8abfc626abec1b2ea3600e
Marcelo <marcelo.bgl@gmail.com>
2019-05-14 01:35:57 -0300

    update 13-05

--- megasena.py
+++ megasena.py
@@ -9,15 +9,14 @@ from selenium.common.exceptions import NoSuchElementException
 driver = webdriver.Chrome(executable_path="C:\selenium\chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
-numero=['']
-numeros=[[''],['']]
+numero=[]
+numeros=[]
+dezenas=[]
 
 ''' Página que iremos acessar '''
 driver.get("http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena")
 
 def ultimo_resultado():
-    print("Try 1 - acessar a página e verificar o último resultado")
-
     try:
         ''' Explicit Waits '''
         page = WebDriverWait(driver, 5).until(
@@ -41,29 +40,21 @@ def ultimo_resultado():
     except NoSuchElementException as erro:
         print("ERRO: Busca pelos campos html.\nCAUSA: Elementos não encontrados.")
         print(erro)
-    finally:
-        print("ultimo_resultado executado.")
-        # fechar_driver()
 
 def resultados_anteriores():
-    print("Try 2 - acessar a página e verificar os resultados anteriores.")
-
     try:
         ultimo_sorteio = ['']
         ''' Recebe STRING com a data do resultado e uma LISTA com as dezenas '''
         ultimo_sorteio = ultimo_resultado()
 
         ############################ DEBUG ##############################
-        print("1º ", ultimo_sorteio[0].text, "\n2º ", ultimo_sorteio[1])
+        # print("1º ", ultimo_sorteio[0].text, "\n2º ", ultimo_sorteio[1])
         #################################################################
 
         regex = re.compile(r'\s\d\d\d\d\s')
         sorteio = regex.findall(ultimo_sorteio[0].text)
 
-        for x in range(1,int(sorteio[0])):
-
-            ######## DEBUG ########
-            print("FOR: Concurso ",x)
+        for num_sorteios in range(1, int(sorteio[0])):
 
             ''' Explicit Waits '''
             page = WebDriverWait(driver, 5).until(
@@ -73,7 +64,7 @@ def resultados_anteriores():
             # driver.executeScript("document.getElementById('buscaConcurso').setAttribute('value', '"+x+"')")
             # driver.execute_script("document.getElementById('buscaConcurso').value='"+str(x)+"'")
             enviaconcurso = driver.find_element_by_id("buscaConcurso")
-            enviaconcurso.send_keys(x)
+            enviaconcurso.send_keys(num_sorteios)
             enviaconcurso.send_keys(Keys.RETURN)
 
             time.sleep(1)
@@ -83,23 +74,22 @@ def resultados_anteriores():
             lista_campo_dezenas = driver.find_element_by_id("ulDezenas")
             ''' percorre os campos html "li" com os valores de cada dezena'''
             lista_numeros = lista_campo_dezenas.find_elements_by_tag_name("li")
-            ''' Pega o valor textual do caralho h2 que escreve "Resultado Concurso" '''
+            ''' Pega o valor textual do campo h2 que escreve "Resultado Concurso" '''
             lista_texto_concurso = driver.find_element_by_xpath("//h2[contains(text(), 'Resultado')]/span[contains(text(), 'Concurso')]")
 
-            for x, i in enumerate(lista_numeros, 0):
-                print(type(lista_numeros))
-                numeros.insert(x,lista_texto_concurso.text+"-"+lista_numeros)
+            ''' Preenche a lista DEZENAS com cada str DEZENA encontrada LISTA_NUMERO '''
+            for indice, dezena in enumerate(lista_numeros):
+                dezenas.insert(indice, dezena.text)
                 ################## DEBUG ################
                 # print(lista_texto_concurso.text, " - ", numero[:])
                 #########################################
-                print(numeros[x])
+
+            print(lista_texto_concurso.text + " {0}-{1}-{2}-{3}-{4}-{5}".format(*dezenas))
+            del dezenas[:]
 
     except NoSuchElementException as erro:
         print("ERRO: Busca pelos campos html.\nCAUSA: Elementos não encontrados.")
         print(erro)
-    finally:
-        print("resultados_anteriores executado.")
-        # fechar_driver()
 
 def fechar_driver():
     driver.quit()
